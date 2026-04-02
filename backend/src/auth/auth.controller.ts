ec72a411d1a1fb96da87950afb23698020ed695a
boatengine <thanapat.boat16@gmail.com>
2026-04-02 14:54:30 +0700

    add: register controller

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -8,13 +8,14 @@ import {
   Delete,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
-  register() {
-    return this.authService.register();
+  register(@Body() data: RegisterDto) {
+    return this.authService.register(data);
   }
 }
